dcd971253fbd3ca0a631d6c15440cc69d55e9b1d
glonpl <bratajczyk@o2.pl>
2019-03-02 17:57:36 +0100

    Creating user schema

---
+++ FinanceHelper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FinanceHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ FinanceHelper/User.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceHelper
+{
+    public interface IUser
+    {
+        #region properties
+        string Nick { get; } 
+        string Password { get; } 
+        #endregion
+        #region methods
+        string CountTendency(); // Bilans -12.34
+        string CountIncomes(); // 12 przychodów wartości 1234zl
+        string CountOutcomes();
+        string ListTransactions();
+        string ShowTransaction(int i);
+        bool DeleteTransaction(int i);
+        void DeleteAllTransactions();
+        #endregion
+    }
+    public class User : IUser
+    {
+        #region properties
+        public string Nick { get; private set; }
+        public string Password { get; private set; }
+        #endregion
+        #region methods
+        public User(string nick, string password)
+        {
+            Nick = nick;
+            Password = password;
+        }
+
+        public string CountTendency()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CountIncomes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CountOutcomes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ListTransactions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ShowTransaction(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteTransaction(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAllTransactions()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
