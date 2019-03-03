e4e23d494637f15d4b64b19b799aeaa4ccd23a32
glonpl <bratajczyk@o2.pl>
2019-03-03 23:36:03 +0100

    Creating Manage Class

---
+++ FinanceHelper/Manage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceHelper
+{
+    class Manage
+    {
+        /*
+         * manage()
+         * logowanie
+         * rejestracja
+         * 
+         */
+    }
+}
